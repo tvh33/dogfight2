7c3c71cb6b7601ef2ade2c8a77c3194f1e7994ae
Tommy Hansen <tvhansenn33@gmail.com>
2012-04-23 00:57:54 +0200

    second

--- conf.lua
+++ conf.lua
@@ -3,6 +3,6 @@ function love.conf(t)
 	t.author = "Tommy V. Hansen"
 	t.screen.width = 1024
 	t.screen.height = 768
-	t.screen.fullscreen = true
+	t.screen.fullscreen = false
 	t.version = "0.1.1"
 end
